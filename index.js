0e38545a38b81f94d4d0ec8e536d223a7ccc4cc6
William Fletcher <wfletch7@gmail.com>
2022-12-01 19:00:26 -0500

    added search v1 functionality

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const AuthRouter = require('./routes/authRoutes')
 const playbackRouter = require('./routes/playbackRoutes')
+const searchRouter = require('./routes/searchRoutes')
 const cors = require('cors')
 
 const port = process.env.PORT
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/auth', AuthRouter.route)
 app.use('/playback', playbackRouter.route)
+app.use('/search', searchRouter.route)
 app.listen(port, () => {
     console.log(`hola, world, PORT: ${port}`)
 })
