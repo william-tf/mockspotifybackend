8620529bc420c03f7543bdf3b16d8ebf6ec96215
seth-bradshaw <seth.bradshaw27@gmail.com>
2023-02-01 12:56:41 -0700

    wrapped routes in trycatch middleware

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,12 +1,13 @@
 const router = require('./index');
 const { getCurrentUserPlaylists, getCurrentUserProfile, getCurrentUserTopItems, getUserSavedTracks } = require('../controllers/profile/profileController');
 const { getPlaylistImages } = require('../controllers/playlist/playlistController');
+const trycatch = require('../middleware/trycatch');
 
-router.get('/playlists', getCurrentUserPlaylists);
-router.get('/playlists', getPlaylistImages);
-router.get('/profile', getCurrentUserProfile);
-router.get('/profile/top', getCurrentUserTopItems);
-router.get('/tracks', getUserSavedTracks)
+router.get('/playlists', trycatch(getCurrentUserPlaylists));
+router.get('/playlists', trycatch(getPlaylistImages));
+router.get('/profile', trycatch(getCurrentUserProfile));
+router.get('/profile/top', trycatch(getCurrentUserTopItems));
+router.get('/tracks', trycatch(getUserSavedTracks))
 
 
 module.exports = { route: router }
