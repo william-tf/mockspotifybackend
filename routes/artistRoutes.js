eeb7d2447819fa3a8b6f3d2940290e7f843ca6f8
seth-bradshaw <seth.bradshaw27@gmail.com>
2024-01-14 12:36:53 -0700

    made these changes 6 months ago

--- routes/artistRoutes.js
+++ routes/artistRoutes.js
@@ -2,7 +2,7 @@ const router = require('./index')
 const { getArtistDetails, getArtistTopTracks, getArtistAlbums } = require('../controllers/artists/index')
 const trycatch = require('../middleware/trycatch')
 
-router.get('/:artist_id', trycatch(getArtistDetails));
+router.get('/:artist_id/details', trycatch(getArtistDetails));
 router.get('/:artist_id/top-tracks', trycatch(getArtistTopTracks));
 router.get('/:artist_id/albums', trycatch(getArtistAlbums));
 
